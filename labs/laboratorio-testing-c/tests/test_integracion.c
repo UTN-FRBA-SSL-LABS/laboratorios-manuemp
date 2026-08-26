d89d6103928b1452bd98a656c833829234d7fd95
manuemp <mpedro@frba.utn.edu.ar>
2026-08-26 02:40:38 +0000

    Ejercicios hasta P12

--- labs/laboratorio-testing-c/tests/test_integracion.c
+++ labs/laboratorio-testing-c/tests/test_integracion.c
@@ -13,17 +13,43 @@
 
 /* TODO: escribir test_compra_con_descuento() siguiendo la guia del .md */
 
+void test_compra_con_descuento(void){
+    Carrito c;
+    carrito_init(&c);
+    Producto pan = {"Pan", 200, 3};
+    Producto leche = {"Leche", 350, 2};
+    carrito_agregar(&c, pan);
+    carrito_agregar(&c, leche);
+    int total = carrito_total(&c);
+    int total_descuento = carrito_descuento(total, 10);
+    ASSERT_IGUAL(1300, total);
+    ASSERT_IGUAL(1170, total_descuento);
+}
+
 /* ═══════════════════════════════════════════════════════════════════════════
  *  PARTE E — Disenar un test propio (ver README.md, Parte 9)
  * ═══════════════════════════════════════════════════════════════════════════ */
 
 /* TODO: escribir test_agregar_hasta_llenar() */
+void test_agregar_hasta_llenar(void){
+    Carrito c;
+    carrito_init(&c);
+    Producto pan = {"Pan", 200, 3};
+    carrito_agregar(&c, pan);
+    carrito_agregar(&c, pan);
+    carrito_agregar(&c, pan);
+    carrito_agregar(&c, pan);
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));
+    ASSERT_IGUAL(0, carrito_agregar(&c, pan));
+    ASSERT_IGUAL(MAX_ITEMS, carrito_contar(&c));
+}
+
 
 int main(void) {
     printf("=== Tests de integracion ===");
     /* Descomentar a medida que agregues las funciones: */
-    /* test_compra_con_descuento();  */
-    /* test_agregar_hasta_llenar();  */
+    test_compra_con_descuento();
+    test_agregar_hasta_llenar();
     RESUMEN();
     return EXIT_CODE();
 }
